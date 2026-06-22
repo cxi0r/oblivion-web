3073f763e78ed5dc074240d6f120de63dbf9fe53
Carlos Ignacio Olave Ruz <127158176+cxi0r@users.noreply.github.com>
2026-06-22 17:44:17 -0400

    Update obfuscate.js

--- api/obfuscate.js
+++ api/obfuscate.js
@@ -23,8 +23,6 @@ export default async function handler(req, res) {
         return res.status(200).json(data);
 
     } catch (error) {
-        console.error(error);
-
         return res.status(500).json({
             success: false,
             error: error.message
