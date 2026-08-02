0d63efac8edf47b9d4512d0c4c4498514d3ca020
Carlos Ignacio Olave Ruz <127158176+cxi0r@users.noreply.github.com>
2026-08-01 22:36:44 -0400

    Update script.js

--- script.js
+++ script.js
@@ -662,7 +662,8 @@ document.addEventListener('DOMContentLoaded', () => {
         "Bloodmoon Slap", "Radioactive Slap", "Rainbow Slap",
         "Rainbow Hammer", "Bloodmoon Hammer", "Radioactive Airstrike",
         "Yin Yang Lamp", "Demon's Head", "Lava Slap", "Lava Blaster",
-        "Alien Slap", "Blackhole Bomb", "Candy Sentry", "Phantom Slap"
+        "Alien Slap", "Blackhole Bomb", "Candy Sentry", "Phantom Slap",
+        "Candy Slap"
     ];
 
     // ============================================================
